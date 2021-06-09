3a9dae045f2cb1249ef627616460cb9c7f52c0c4
Yashvardhan <yashvardhan.gla_cs18@gla.ac.in>
2021-06-10 00:21:44 +0530

    q2

---
+++ src/DisplayArrayReverse.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class DisplayArrayReverse{
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        Scanner scn=new Scanner(System.in);
+        int n=scn.nextInt();
+        int [] arr=new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=scn.nextInt();
+        }
+        displayArr(arr,0);
+    }
+
+    public static void displayArr(int[] arr, int idx){
+        if(idx==arr.length){
+            return;
+        }
+        displayArr(arr,idx+1);
+        System.out.println(arr[idx]);
+
+
+    }
+
+}
