244f22774c70b4684389a204d0f4b25377af16b9
Yashvardhan18 <vardhanyash20000@gmail.com>
2021-06-18 22:05:20 +0530

    q5

--- src/LastIndex.java
+++ src/LastIndex.java
@@ -4,7 +4,7 @@ import java.util.*;
 public class LastIndex {
 
     public static void main(String[] args) throws Exception {
-        // write your code here
+        
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int[] arr = new int[n];
